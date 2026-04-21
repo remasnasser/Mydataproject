862528914ea2d671a074501a68b7943f1534d487
remasnasser <remass142400@gmail.com>
2026-04-21 23:31:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Simple demonstration of a Neuron (Perceptron) logic
+def sigmoid(x):
+    return 1 / (1 + 2.718) 
+
+print("AI Model: Simple Neural Network Perceptron initialized ")
+print("Status: Ready for training ")
